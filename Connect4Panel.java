3e0d7d13f1c16db7f3089b088ba7e1c977a21982
deborahbakshiyev <deborah.bakshiyev@gmail.com>
2014-12-11 17:06:11 -0500

    uhhhh

--- Connect4Panel.java
+++ Connect4Panel.java
@@ -5,23 +5,26 @@ import javax.swing.border.*;
 
 class Connect4Panel extends JPanel implements ActionListener {
 
-	Connect4Board gameBoard;
-	JButton powerButton;
+ Connect4Board gameBoard;
+ JButton powerButton;
 
-	Connect4Panel() {
+ Connect4Panel() {
 
-		super(new FlowLayout(FlowLayout.CENTER, 50, 50));
-		gameBoard = new Connect4Board(this);
-		powerButton = new JButton("Power");
-		add(gameBoard);
-		add(powerButton);
-		powerButton.addActionListener(this);
-	}
+  super(new FlowLayout(FlowLayout.CENTER, 50, 50));
+  gameBoard = new Connect4Board(this);
+  powerButton = new JButton("Power");
+  add(gameBoard);
+  add(powerButton);
+  powerButton.addActionListener(this);
+  
+ }
 
-	public void actionPerformed (ActionEvent e) {
+ public void actionPerformed (ActionEvent e) {
 
-		if (e.getSource().equals(powerButton)) {
-			gameBoard.setEnabled(!gameBoard.isEnabled());
-		}
-	}
+  if (e.getSource().equals(powerButton)) {
+   gameBoard.setEnabled(!gameBoard.isEnabled());
+  }
+  
+ }
+ 
 }
