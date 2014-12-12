b2b3e99398b6f422d16e4e7dfdf8eb41199f002f
Kevin Mei <kevinm.aero@gmail.com>
2014-12-11 22:16:44 -0500

    title

--- Connect4Panel.java
+++ Connect4Panel.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import javax.swing.border.*;
 
 class Connect4Panel extends JPanel implements ActionListener {
+<<<<<<< Updated upstream
 
  Board gameBoard;
  JButton powerButton;
@@ -18,15 +19,37 @@ class Connect4Panel extends JPanel implements ActionListener {
   add(gameBoard);
   add(powerButton);
   powerButton.addActionListener(this);
+=======
+>>>>>>> Stashed changes
   
- }
-
- public void actionPerformed (ActionEvent e) {
-
-  if (e.getSource().equals(powerButton)) {
-   gameBoard.setEnabled(!gameBoard.isEnabled());
+  Board gameBoard;
+  JButton powerButton;
+  JLabel title;
+  
+  Connect4Panel() {
+    
+    super(new FlowLayout(FlowLayout.CENTER, 100, 100));
+    setBackground(new Color(172,209,233));
+    title = new JLabel("CONNECT FOUR");
+    title.setFont(new Font("SANS_SERIF", Font.BOLD, 72));
+    title.setForeground(Color.WHITE);
+    title.setAlignmentX(0.5f);
+    gameBoard = new Board();
+    gameBoard.border();
+    powerButton = new JButton("New Game");
+    add(title);
+    add(gameBoard);
+    add(powerButton);
+    powerButton.addActionListener(this);
+    
   }
   
- }
- 
+  public void actionPerformed (ActionEvent e) {
+    
+    if (e.getSource().equals(powerButton)) {
+      gameBoard.setEnabled(!gameBoard.isEnabled());
+    }
+    
+  }
+  
 }
