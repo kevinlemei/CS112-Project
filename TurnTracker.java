00a0882f811305b3cd3b943857284c9c0168d431
deborahbakshiyev <deborah.bakshiyev@gmail.com>
2014-12-12 18:10:41 -0500

    sans serif

--- TurnTracker.java
+++ TurnTracker.java
@@ -13,7 +13,7 @@ class TurnTracker extends JLabel {
     setPreferredSize (new Dimension(200,50));
     
     currentTurn = new JLabel("Red's turn");
-    currentTurn.setFont(new Font("SERIF", Font.BOLD, 36));
+    currentTurn.setFont(new Font("SANS_SERIF", Font.BOLD, 24));
     currentTurn.setForeground(Color.RED);
     add(currentTurn);
 
